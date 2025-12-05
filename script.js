5dc146650469fe3264c49251ee7fe58f1353e46a
itu-itis25-myagmar25 <myagmar25@itu.edu.tr>
2025-12-05 19:54:40 +0300

    Refactor button retrieval to changeColor function
    Moved button element retrieval inside changeColor function.

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const colors = ["red", "blue", "green", "gray"];
 let currentIndex = 0;
 
-const btn = document.getElementById("colorBtn");
-
-btn.addEventListener("click", changeColor);
-
 function changeColor() {
+    const btn = document.getElementById("colorBtn");
     btn.style.backgroundColor = colors[currentIndex];
     currentIndex++;
 
